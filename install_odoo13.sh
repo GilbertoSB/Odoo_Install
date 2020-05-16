531dcfe8e358b87ca2c90a48f30de1b9a1807ada
gsalguero <gilbertosalguero@hotmail.com>
2020-05-15 22:20:07 -0600

    Pillow
    Pillow

--- install_odoo13.sh
+++ install_odoo13.sh
@@ -76,9 +76,10 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 #--------------------------------------------------
 echo -e "\n--- Installing Python 3 + pip3 --"
 sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 gdebi -y
+sudo python3 -m pip install --upgrade Pillow
 
 echo -e "\n---- Install python packages/requirements ----"
-sudo -H pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
+sudo -H pip3 install -r https://raw.githubusercontent.com/GilbertoSB/odoo13-course/develop/requirements.txt
 
 echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
 sudo apt-get install nodejs npm -y
